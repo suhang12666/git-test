9a7e857b98b72be953e49f67c81d0ebf1bf2e60a
Layne <1609952921@qq.com>
2022-11-25 20:55:47 +0800

    测试

---
+++ src/com/bdqn/Su.java
@@ -0,0 +1,12 @@
+package com.bdqn;
+
+public class Su {
+    public static void main(String[] args) {
+        System.out.println(123);
+        System.out.println(1235213);
+        System.out.println(123535325);
+        System.out.println(123432323);
+        System.out.println("vbbbbbbbbbbbbbb");
+
+    }
+}
